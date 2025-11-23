16728ede6c2e907d5a0a89037230e576ef8fc764
18613804226 <5538072662@qq.com>
2025-11-24 00:54:38 +0800

    faet: add Environment Variables

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 async function bootstrap() {
-  
+  // / 根据环境设置 CORS
+  const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:5777';
   const app = await NestFactory.create(AppModule);
-   // ✅ 正确方式：使用 Nest 内置方法
+  // ✅ 正确方式：使用 Nest 内置方法
   app.enableCors({
-    origin: 'http://localhost:5777', // 前端地址
+    origin: frontendUrl, // 前端地址
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
