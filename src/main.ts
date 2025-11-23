c401f338881bd50874dd4e410eefb0ef36312aa0
18613804226 <5538072662@qq.com>
2025-11-24 01:34:43 +0800

    fix: error

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import * as fs from 'fs';
 async function bootstrap() {
+  // 根据 NODE_ENV 决定是否加载 .env 文件
+  const envFile =
+    process.env.NODE_ENV === 'development'
+      ? '.env.development'
+      : '.env.production';
+
+  if (envFile && fs.existsSync(envFile)) {
+    dotenv.config({ path: envFile });
+  }
   // / 根据环境设置 CORS
   const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:5777';
   const app = await NestFactory.create(AppModule);
