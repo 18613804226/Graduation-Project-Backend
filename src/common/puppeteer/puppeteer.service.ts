f3ec780a178f9ca09d3658dc97267fbb932e3721
18613804226 <5538072662@qq.com>
2025-11-30 21:37:12 +0300

    fix: build mistake

--- src/common/puppeteer/puppeteer.service.ts
+++ src/common/puppeteer/puppeteer.service.ts
@@ -1,8 +1,8 @@
 // src/common/puppeteer/puppeteer.service.ts
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import * as puppeteer from 'puppeteer';
-import * as fs from 'fs';
-import * as path from 'path';
+// import * as fs from 'fs';
+// import * as path from 'path';
 
 @Injectable()
 export class PuppeteerService implements OnModuleInit, OnModuleDestroy {
@@ -10,29 +10,29 @@ export class PuppeteerService implements OnModuleInit, OnModuleDestroy {
 
   async onModuleInit() {
     // 尝试自动查找 Puppeteer 下载的 Chrome 路径
-    let executablePath = puppeteer.executablePath();
-    console.log('🔍 __dirname:', __dirname);
-    console.log(
-      '🔍 Project root (3x ..):',
-      path.resolve(__dirname, '..', '..', '..'),
-    );
-    console.log('🔍 Chrome path:', executablePath);
-    console.log('✅ File exists?', fs.existsSync(executablePath));
-    // 如果默认路径不存在（Render 环境常见），手动构造路径
-    if (!fs.existsSync(executablePath)) {
-      // const platform = 'linux'; // Render 是 Linux
-      // const version = '142.0.7444.175'; // 你的错误日志中的版本
-      executablePath = path.resolve(
-        __dirname,
-        '..',
-        '..',
-        '..', // → /opt/render/project/src
-        '.local-chromium',
-        'chrome',
-        'linux-142.0.7444.175',
-        'chrome',
-      );
-    }
+    let executablePath = '/usr/bin/chromium-browser';
+    // console.log('🔍 __dirname:', __dirname);
+    // console.log(
+    //   '🔍 Project root (3x ..):',
+    //   path.resolve(__dirname, '..', '..', '..'),
+    // );
+    // console.log('🔍 Chrome path:', executablePath);
+    // console.log('✅ File exists?', fs.existsSync(executablePath));
+    // // 如果默认路径不存在（Render 环境常见），手动构造路径
+    // if (!fs.existsSync(executablePath)) {
+    //   // const platform = 'linux'; // Render 是 Linux
+    //   // const version = '142.0.7444.175'; // 你的错误日志中的版本
+    //   executablePath = path.resolve(
+    //     __dirname,
+    //     '..',
+    //     '..',
+    //     '..', // → /opt/render/project/src
+    //     '.local-chromium',
+    //     'chrome',
+    //     'linux-142.0.7444.175',
+    //     'chrome',
+    //   );
+    // }
 
     this.browser = await puppeteer.launch({
       executablePath,
