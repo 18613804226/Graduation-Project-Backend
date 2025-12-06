4362d2f05a42333633d9b75e108a4314a577b70d
18613804226 <5538072662@qq.com>
2025-12-06 23:33:17 +0300

    fix: build mistake

--- prisma/migrations/20251206001156_add_course_id_to_exam_template/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "ExamTemplate" ADD COLUMN     "courseId" INTEGER NOT NULL DEFAULT 0;
-
--- AddForeignKey
-ALTER TABLE "ExamTemplate" ADD CONSTRAINT "ExamTemplate_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "Course"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
